d6536ab549baba60ed72768ee07ee6ff4b5f2be2
Anurag <anurag8731@gmail.com>
2026-02-05 13:04:48 +0530

    "Todo App Day 4 - LocalStorage persistence"

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [task, setTask] = useState("");
-  const [todos, setTodos] = useState([]);
+
+  // Load from localStorage on start
+  const [todos, setTodos] = useState(() => {
+    const saved = localStorage.getItem("todos");
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  // Save to localStorage when todos change
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const addTodo = () => {
     if (task.trim() === "") return;
 
     const newTodo = {
-        id: Date.now(),
-        text: task,
-        completed: false
+      id: Date.now(),
+      text: task,
+      completed: false,
     };
 
     setTodos([...todos, newTodo]);
@@ -23,13 +33,11 @@ function App() {
 
   const toggleComplete = (id) => {
     setTodos(
-        todos.map(todo =>
-            todo.id === id
-            ? {...todo, completed : !todo.completed}
-            : todo
-        )
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo,
+      ),
     );
-  }
+  };
 
   return (
     <div style={{ padding: 20 }}>
@@ -42,19 +50,22 @@ function App() {
         onChange={(e) => setTask(e.target.value)}
       />
 
-      <button style={{margin : 10 }} onClick={addTodo}>Add</button>
+      <button style={{ margin: 10 }} onClick={addTodo}>
+        Add
+      </button>
 
       {todos.map((todo) => (
         <div key={todo.id}>
-          <span onClick={() => toggleComplete(todo.id)}
+          <span
+            onClick={() => toggleComplete(todo.id)}
             style={{
-                textDecoration : todo.completed
-                ? "line-through"
-                : "none",
-                cursor : "pointer",
-                marginRight : 10
+              textDecoration: todo.completed ? "line-through" : "none",
+              cursor: "pointer",
+              marginRight: 10,
             }}
-            >{todo.text}</span>
+          >
+            {todo.text}
+          </span>
           <button onClick={() => deleteTodo(todo.id)}>Delete</button>
         </div>
       ))}
