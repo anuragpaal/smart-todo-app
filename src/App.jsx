82e269b721c4bf15587412f4f25dd90636d71279
Anurag <anurag8731@gmail.com>
2026-02-05 12:27:54 +0530

    Todo App Day 2 - Delete feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,46 @@
 import { useState } from "react";
 
 function App() {
-    const [task,setTask] = useState("");
-    const [todos,setTodos] = useState([]);
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
 
-    const addTodo  = () => {
-        if(task.trim() === "") return;
+  const addTodo = () => {
+    if (task.trim() === "") return;
 
-        setTodos([...todos,task]);
-        setTask("")
-    }
+    const newTodo = {
+        id: Date.now(),
+        text: task
+    };
 
-    return(
-        <div style={{padding : 20 }}>
-            <h1>Smart Todo App</h1>
+    setTodos([...todos, newTodo]);
+    setTask("");
+  };
 
-            <input 
-            type="text"
-            placeholder="Enter Text.."
-            value={task}
-            onChange={(e) => setTask(e.target.value)}
-            />
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
-            <button style={{margin : 10}} onClick={addTodo }>Add</button>
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Smart Todo App</h1>
 
-            {todos.map((t,i) => (
-                <p key={i}>{t}</p>
-            ))}
+      <input
+        type="text"
+        placeholder="Enter Task..."
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+
+      <button style={{margin : 10 }} onClick={addTodo}>Add</button>
+
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <span>{todo.text}</span>
+          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
 export default App;
